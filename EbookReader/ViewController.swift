a53608f6e6dfb0f469e58014354664f2058c2bcd
Ten Ol <tennet0505@gmail.com>
2020-04-08 13:53:38 +0600

    first commit

--- EbookReader/ViewController.swift
+++ EbookReader/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        
+        
     }
+    
 
 
+    
 }
 
